093c8f7499094cdaa4d9c16e0ff0a339edcc888e
pie <104147789+pie2457@users.noreply.github.com>
2024-01-12 23:33:32 +0900

    Title: 정상적인 구구단, Time: 143ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240112/정상적인 구구단/normal-multiple.java
+++ 240112/정상적인 구구단/normal-multiple.java
@@ -1,7 +1,12 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i < 4; i++) {
-            for (int j = 1; j < 4; j++) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
                 System.out.print(i + " * " + j + " = " + (i * j));
                 if (j < 3) 
                     System.out.print(", ");
