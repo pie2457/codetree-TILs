a772fa7d6b2be4298eb43627d5a6acd0618e9d58
pie <104147789+pie2457@users.noreply.github.com>
2024-01-18 02:23:02 +0900

    Title: 알파벳 출력하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240118/알파벳 출력하기/print-alphabet.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        char c = 'A';
+
+        for(int i = 0; i < n; i++) {
+            for(int j = n - i; j <= n; j++) {
+                System.out.print(c);
+                c++;
+                if (c > 'Z') {
+                    c = 'A';
+                }
+            }
+            System.out.println();
+        }
+    }
+}
