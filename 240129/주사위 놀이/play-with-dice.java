e273127382dee3ad1bca643165118b80522f06b7
pie <104147789+pie2457@users.noreply.github.com>
2024-01-29 20:32:16 +0900

    Title: 주사위 놀이, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240129/주사위 놀이/play-with-dice.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int arr[] = new int[7];
+        for(int i = 0; i < 10; i++) {
+            int n = sc.nextInt();
+
+            arr[n]++;
+        }
+
+        for(int i = 1; i < arr.length; i++) {
+            System.out.println(i + " - " + arr[i]);
+        }
+    }
+}
