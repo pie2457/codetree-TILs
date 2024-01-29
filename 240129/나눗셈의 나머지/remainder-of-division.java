03a48f484fbe4e029201359f9c4db2ae29d6069e
pie <104147789+pie2457@users.noreply.github.com>
2024-01-29 22:18:14 +0900

    Title: 나눗셈의 나머지, Time: 147ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240129/나눗셈의 나머지/remainder-of-division.java
+++ 240129/나눗셈의 나머지/remainder-of-division.java
@@ -7,14 +7,14 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        int arr[] = new int[10];
+        int arr[] = new int[11];
         int sum = 0;
 
         while (a >= 1) {
             arr[a % b]++;
             a = a / b;
         }
-        for(int i = 0; i < 10; i++) {
+        for(int i = 0; i < 11; i++) {
             sum += Math.pow(arr[i], 2);
         }
         System.out.print(sum);
