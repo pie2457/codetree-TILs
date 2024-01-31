57cf139f1c2b60f206050c02a5ccb4840ea63ed1
pie <104147789+pie2457@users.noreply.github.com>
2024-01-31 16:11:08 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240131/배열 놀이/play-with-array.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+
+        int a, b, c = 0;
+        int arr[] = new int[n + 1];
+
+        for(int i = 1; i <= n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < q; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            if (i == q - 1) {
+                c = sc.nextInt();
+            }
+            if (a == 1) {
+                System.out.println(arr[b]);
+            } else if(a == 2) {
+                for(int j = 1; j <= arr.length; j++) {
+                    if (arr[j] == b) {
+                        System.out.println(j);
+                        break;
+                    }
+                }
+            } else if (a == 3) {
+                for(int j = b; j <= c; j++) {
+                    System.out.print(arr[j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
