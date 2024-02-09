4a491444c0da84d7977dd15d3c5b8357da98311f
pie <104147789+pie2457@users.noreply.github.com>
2024-02-09 23:21:41 +0900

    Title: 격자에 점 그리기, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240209/격자에 점 그리기/draw-points-on-grid.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int cnt = 1;
+
+        int arr[][] = new int[n+1][n+1];
+
+        for(int i = 0; i < m; i++) {
+            int r = sc.nextInt();
+            int c = sc.nextInt();
+
+            arr[r][c] = cnt++;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
