f8e6c351d3ff754606386edfa3ae625020fb72ad
pie <104147789+pie2457@users.noreply.github.com>
2024-02-17 19:31:26 +0900

    Title: Run Length 인코딩, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240217/Run Length 인코딩/run-length-encoding.java
+++ 240217/Run Length 인코딩/run-length-encoding.java
@@ -5,22 +5,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String s = sc.next();
-        int cnt = 0;
+        int cnt = 1;
+        char c = s.charAt(0);
         String result = "";
 
         for(int i = 1; i < s.length(); i++) {
-            if (s.charAt(i - 1) == s.charAt(i)) {
+            if (s.charAt(i) == c) {
                 cnt++;
             } else {
-                result += s.charAt(i-1);
-                result += cnt + 1;
-                cnt = 0;
-            }
-            if (s.length() - 1 == i) {
-                result += s.charAt(i);
-                result += cnt + 1;
+                result += c;
+                result += cnt;
+                c = s.charAt(i);
+                cnt = 1;
             }
         }
+        result += c;
+        result += cnt;
         System.out.println(result.length());
         System.out.println(result);
     }
