fb6c7f7d0da34380a04817d7f6703201e62cba3b
pie <104147789+pie2457@users.noreply.github.com>
2024-02-19 18:03:06 +0900

    Title: 문자 교체하기, Time: 250ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240219/문자 교체하기/changing-char.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+        char arr[] = b.toCharArray();
+
+        arr[0] = a.charAt(0);
+        arr[1] = a.charAt(1);
+
+        System.out.print(String.valueOf(arr));
+    }
+}
