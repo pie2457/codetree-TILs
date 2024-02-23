18c464487f187d3595069e59b8f5e6dd461ddffc
pie <104147789+pie2457@users.noreply.github.com>
2024-02-23 15:58:54 +0900

    Title: 그 다음 알파벳, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240223/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.next().charAt(0);
+        int n = c + 1;
+
+        System.out.print((char)n);
+    }
+}
