193fc09a11a81db12389a2bb5c45b3b75fe70805
pie <104147789+pie2457@users.noreply.github.com>
2024-02-24 15:58:57 +0900

    Title: 소문자와 숫자, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240224/소문자와 숫자/letter-and-number.java
+++ 240224/소문자와 숫자/letter-and-number.java
@@ -4,13 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String s = sc.next();
+        String s = sc.next().toLowerCase();
 
         for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c >= 'A' && c  <= 'Z') {
-                System.out.print((char)(c - 'A' + 'a'));
-            } else if (c >= 'a' && c <= 'z') {
+            
+            if (c >= 'a' && c <= 'z') {
                 System.out.print(c);
             } else if (c >= '0' && c <= '9') {
                 System.out.print(c);
