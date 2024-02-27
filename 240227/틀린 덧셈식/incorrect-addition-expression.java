1bb5c3c4f7b8953d834c01bcbde6f585670c5da7
pie <104147789+pie2457@users.noreply.github.com>
2024-02-27 15:57:34 +0900

    Title: 틀린 덧셈식, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240227/틀린 덧셈식/incorrect-addition-expression.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 123;
+        int k = 43;
+        int h = 80;
+        
+        System.out.print(n + " + " + k + " = " + h );
+    }
+}
