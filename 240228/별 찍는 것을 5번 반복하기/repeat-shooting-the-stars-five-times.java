90b7416ade7117c09252f6426b00aa4b15a336fe
pie <104147789+pie2457@users.noreply.github.com>
2024-02-28 16:13:35 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240228/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        printStar();
+        printStar();
+        printStar();
+        printStar();
+        printStar();
+    }
+
+    public static void printStar() {
+        System.out.println("**********");
+    }
+}
