79b7382de600788a12d2918100edb032565f08ab
pie <104147789+pie2457@users.noreply.github.com>
2024-02-28 23:18:22 +0900

    Title: 사칙연산 함수, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240228/사칙연산 함수/quadratic-operations-function.java
+++ 240228/사칙연산 함수/quadratic-operations-function.java
@@ -5,26 +5,27 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
-        String s = sc.next();
+        String s = sc.next().replaceAll(" ", "");
         int b = sc.nextInt();
+
         if (calculator(a, b, s) == 0) {
             System.out.println("False");
         } else {
-            System.out.println(a + " " + s + " " + b + " = " + calculator(a, b, s));
+            System.out.print(a + " " + s + " " + b + " = ");
+            System.out.print(calculator(a, b, s));
         }
     }
 
-    static int calculator(int a, int b, Stirng s) {
-        if (s == "+") {
+    static int calculator(int a, int b, String s) {
+        if (s.equals("+")) {
             return a + b;
-        } else if (s == "-") {
+        } else if (s.equals("-")) {
             return a - b;
-        } else if (s == "*") {
+        } else if (s.equals("*")) {
             return a * b;
-        } else ig (s == "/") {
+        } else if (s.equals("/")) {
             return a / b;
-        } else {
-            return 0;
         }
+        return 0;
     }
 }
