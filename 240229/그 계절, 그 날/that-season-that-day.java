8f878558751f312f431bdd90a156ea6463c8c372
pie <104147789+pie2457@users.noreply.github.com>
2024-02-29 15:55:18 +0900

    Title: 그 계절, 그 날, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240229/그 계절, 그 날/that-season-that-day.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int y = sc.nextInt();
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+
+        if (month(m) && day(y, m, d)) {
+            System.out.println(season(m));
+        } else {
+            System.out.println(-1);
+        }
+    }
+
+    static boolean year(int y) {
+        if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    static boolean day(int y,int m, int d) {
+        if (year(y) && m == 2 && d <= 29) {
+            return true;
+        } else if (!year(y) && m == 2 && d <= 28) {
+            return true;
+        }
+        if (d <= 31 && m == 1 || m == 3 || m == 5 || m == 7 ||
+        m == 8 || m == 10 || m == 12) {
+            return true;
+        } else if (d <= 30 && m == 4 || m == 6 || m == 9 || m == 11) {
+            return true;
+        }
+        return false;
+    }
+
+    static boolean month(int m) {
+        return m <= 12;
+    }
+
+    static String season(int m) {
+        if (m >= 3 && m <= 5) {
+            return "Spring";
+        } else if (m >= 6 && m <= 8) {
+            return "Summber";
+        } else if (m >= 9 && m <= 11) {
+            return "Fall";
+        } else {
+            return "Winter";
+        }
+    }
+}
