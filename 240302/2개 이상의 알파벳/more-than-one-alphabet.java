f4b64f7d134cc042d2efaee38d4d5a2e7cb905c3
pie <104147789+pie2457@users.noreply.github.com>
2024-03-02 19:07:44 +0900

    Title: 2개 이상의 알파벳, Time: 113ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240302/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        different(s);
+    }
+
+    static void different(String s) {
+        int cnt = 0;
+        for(int i = 1; i < s.length(); i++) {
+            if (s.charAt(i-1) != s.charAt(i)) {
+                cnt++;
+            }
+        }
+        if (cnt >= 2) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
