163de4441460e60bb65d836aff8181e896575801
pie <104147789+pie2457@users.noreply.github.com>
2024-03-03 14:45:20 +0900

    Title: 특정 구간의 합, Time: 256ms, Memory: 10MB, Status: Passed - CodeTree

--- 240303/특정 구간의 합/sum-of-specific-intervals.java
+++ 240303/특정 구간의 합/sum-of-specific-intervals.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Main {
-    public static int arr[] = new int[100];
+    public static int arr[];
     public static int sum = 0;
 
     public static void main(String[] args) {
@@ -9,6 +9,7 @@ public class Main {
 
         int n = sc.nextInt();
         int m = sc.nextInt();
+        arr = new int[n];
 
         for(int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
