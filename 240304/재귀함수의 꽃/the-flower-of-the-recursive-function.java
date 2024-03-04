8e2c65fc2fa66acf4675841643fb47c6c1ab7e3e
pie <104147789+pie2457@users.noreply.github.com>
2024-03-04 15:37:05 +0900

    Title: 재귀함수의 꽃, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240304/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        printer(n);
+    }
+
+    static void printer(int n) {
+        for(int i = n; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        for(int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
