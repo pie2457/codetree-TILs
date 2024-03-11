4889f92b61cccd12a91e3beade1e4a258b5c95f0
pie <104147789+pie2457@users.noreply.github.com>
2024-03-11 16:37:08 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240311/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(calc(arr));
+    }
+
+    static int calc(int arr[]) {
+        int result = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            result = lcm(result, arr[i]);
+        }
+        return result;
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+}
