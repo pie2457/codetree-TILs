eccd59757b5b0b9b42ed3346aec11e4fe6249a68
pie <104147789+pie2457@users.noreply.github.com>
2024-03-15 16:52:19 +0900

    Title: 비오는 날, Time: 334ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 240315/비오는 날/rainy-day.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.time.format.DateTimeFormatter;
+import java.time.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        LocalDate localDate[] = new LocalDate[n];
+
+        int cnt = 0;
+        LocalDateTime local = LocalDateTime.now();
+        String now = local.format(
+            DateTimeFormatter.ofPattern("2100-12-31"));
+
+        for(int i = 0; i < n; i++) {
+            String date = sc.next();
+            String day = sc.next();
+            String weather = sc.next();
+
+            localDate[i] = new LocalDate(date, day, weather);
+
+            if (weather.equals("Rain") && date.compareTo(now) == -1) {
+                now = date;
+                cnt = i;
+            }
+        }
+
+        System.out.println(localDate[cnt].date + " " + localDate[cnt].day + " " + localDate[cnt].weather);
+    }
+
+    static class LocalDate {
+
+        String date;
+        String day;
+        String weather;
+
+        public LocalDate(String date, String day, String weather) {
+            this.date = date;
+            this.day = day;
+            this.weather = weather;
+        }
+    }
+}
