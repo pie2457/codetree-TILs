07a7d3b5003c8104559de3b72b48ee41e6f75cf0
pie <104147789+pie2457@users.noreply.github.com>
2024-03-18 16:14:14 +0900

    Title: 줄 세우기, Time: 306ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240318/줄 세우기/line-up-students.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Student students[] = new Student[n];
+        int num = 1;
+
+        for(int i = 0; i < n; i++) {
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+
+            students[i] = new Student(h, w, num++);
+        }
+
+        Arrays.sort(students);
+
+        for(Student sd : students) {
+            System.out.println(sd.h + " " + sd.w + " " + sd.num);
+        }
+    }
+
+    static class Student implements Comparable<Student> {
+
+        int h, w, num;
+
+        public Student(int h, int w, int num) {
+            this.h = h;
+            this.w = w;
+            this.num = num;
+        }
+        
+        @Override
+        public int compareTo(Student student) {
+            if (this.h != student.h) {
+                return student.h - this.h;
+            }
+            if (this.w != student.w) {
+                return student.w - this.w;
+            }
+            return this.num - student.num;
+        }
+    }
+}
