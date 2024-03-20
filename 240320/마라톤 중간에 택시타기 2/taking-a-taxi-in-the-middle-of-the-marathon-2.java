b522e3d2b4ca43dcfe419a6e80d96f196962c3ce
pie <104147789+pie2457@users.noreply.github.com>
2024-03-20 18:53:16 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 240320/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
+++ 240320/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
@@ -4,29 +4,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int arr[][] = new int[n][2];
+		int n = sc.nextInt();
+		int arr[][] = new int[n][2];
 
-        for(int i = 0; i < n; i++) {
-            arr[i][0] = sc.nextInt();
-            arr[i][1] = sc.nextInt();
-        }
-
-        int min = Integer.MAX_VALUE;
-
-        for(int i = 1; i < n-1; i++) {
-			int sum = 0;
-			int x = Math.abs(arr[0][0] - arr[i][0])
-				+ Math.abs(arr[0][1] - arr[i][1]);
-			int y = Math.abs(arr[i][0] - arr[n-1][0])
-				+ Math.abs(arr[i][1] - arr[n-1][1]);
-
-			sum = Math.abs(x) + Math.abs(y);
-
-			if (min > sum) {
-				min = sum;
-			}
+		for(int i = 0; i < n; i++) {
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
 		}
-        System.out.println(min);
+
+		int min = Integer.MAX_VALUE;
+		int sum = 0;
+
+		for(int i = 0; i < n-1; i++) {
+			int m = Math.abs(arr[i][0] - arr[i + 1][0]) + Math.abs(arr[i][1] - arr[i + 1][1]);
+			sum += Math.abs(m);
+		}
+
+		for(int i = 1; i < n-1; i++) {
+			int prev = Math.abs(arr[i-1][0] - arr[i][0]) + Math.abs(arr[i-1][1] - arr[i][1]);
+			int next = Math.abs(arr[i][0] - arr[i+1][0]) + Math.abs(arr[i][1] - arr[i+1][1]);
+
+			int mm = prev + next;
+			int m = Math.abs(arr[i-1][0] - arr[i+1][0]) + Math.abs(arr[i-1][1] - arr[i+1][1]);
+
+			min = Math.min(min, sum - mm + m);
+		}
+		System.out.println(min);
     }
 }
