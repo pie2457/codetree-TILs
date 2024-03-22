4c08918562e9d465336d9223afa07828c93afc86
pie <104147789+pie2457@users.noreply.github.com>
2024-03-22 14:54:39 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 168ms, Memory: 10MB, Status: Passed - CodeTree

--- 240322/괄호 쌍 만들어주기 2/pair-parentheses-2.java
+++ 240322/괄호 쌍 만들어주기 2/pair-parentheses-2.java
@@ -4,19 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        char arr[] = sc.next().toCharArray();
-
-        char c1 = '(';
-        char c2 = ')';
+        char chars[] = sc.next().toCharArray();
         int cnt = 0;
 
-        for(int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] == c1 && arr[i+1] == c1) {
-                cnt++;
-            } else if (arr[i] == c2 && arr[i+1] == c2) {
-                cnt++;
+        for(int i = 0; i < chars.length - 1; i++) {
+            if (chars[i] == '(' && chars[i+1] == '(') {
+                for(int j = i + 1; j < chars.length - 1; j++) {
+                    if (chars[j] == ')' && chars[j+1] == ')') {
+                        cnt++;
+                    }
+                }
             }
         }
-        System.out.println(cnt % 2 == 0 ? cnt : cnt - 1);
+        System.out.println(cnt);
     }
 }
