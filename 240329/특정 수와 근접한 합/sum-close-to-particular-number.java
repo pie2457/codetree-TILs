09813ca80e079c76ba76f597c427e46359a6fe29
pie <104147789+pie2457@users.noreply.github.com>
2024-03-29 16:04:46 +0900

    Title: 특정 수와 근접한 합, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240329/특정 수와 근접한 합/sum-close-to-particular-number.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int s = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        int sum = 0;
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int all = sum;
+                sum -= arr[i] + arr[j];
+
+                if (sum - s < min) {
+                    min = sum - s;
+                }
+                sum = all;
+            }
+        }
+        System.out.println(min);
+    }
+}
