60569cb1b96031e8cf16dc768d8163956c42fcef
pie <104147789+pie2457@users.noreply.github.com>
2024-04-05 16:43:46 +0900

    Title: 황금비율 토스트, Time: 1123ms, Memory: 36MB, Status: Passed - CodeTree

--- 240405/황금비율 토스트/golden-toast.java
+++ 240405/황금비율 토스트/golden-toast.java
@@ -4,38 +4,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        LinkedList<Charactor> ll = new LinkedList<>();
-        int m = sc.nextInt();
-        String s = sc.next();
+		int n = sc.nextInt();
+		LinkedList<Character> ll = new LinkedList<>();
+		int m = sc.nextInt();
+		String s = sc.next();
 
-        for(int i = 0; i < s.length(); i++) {
-            ll.add(s.charAt(i));
-        }
+		for(int i = 0; i < s.length(); i++) {
+			ll.add(s.charAt(i));
+		}
 
-        ListIterator<Character> it = ll.listIterator(ll.size());
+		ListIterator<Character> it = ll.listIterator(ll.size());
 
-        for(int i = 0; i < m; i++) {
-            String s1 = sc.nextLine();
+		for(int i = 0; i < m; i++) {
+			String s1 = sc.next();
 
-            if (s.equals("L") && it.hasPrevious()) {
-                it.previous();
-            } else if (s.equals("R") && it.hasNext()) {
-                it.next();
-            } else if (s.equals("D") && it.hasNext()) {
-                it.next();
-                it.remove();
-            } else if (s.contains("P")) {
-                char c = s.split(" ", 2);
-                it.add(c);
-            }
-        }
-        while(it.hasPrevious()) {
-            it.previous();
-        }
+			if (s1.equals("L") && it.hasPrevious()) {
+				it.previous();
+			} else if (s1.equals("R") && it.hasNext()) {
+				it.next();
+			} else if (s1.equals("D") && it.hasNext()) {
+				it.next();
+				it.remove();
+			} else if (s1.contains("P")) {
+				char c = sc.next().charAt(0);
+				it.add(c);
+			}
+		}
+		while(it.hasPrevious()) {
+			it.previous();
+		}
 
-        while(it.hasNext()) {
-            System.out.print(it.next());
-        }
+		while(it.hasNext()) {
+			System.out.print(it.next());
+		}
     }
 }
