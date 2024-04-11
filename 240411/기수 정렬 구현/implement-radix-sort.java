fba2c47aae6184d983dcefc8c26a4629fa7640e0
pie <104147789+pie2457@users.noreply.github.com>
2024-04-11 16:07:02 +0900

    Title: 기수 정렬 구현, Time: 1690ms, Memory: 52MB, Status: Passed - CodeTree

--- 240411/기수 정렬 구현/implement-radix-sort.java
+++ 240411/기수 정렬 구현/implement-radix-sort.java
@@ -1,45 +1,46 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static final int MAX_N = 100000;
+    public static final int MAX_K = 6;
+    public static final int MAX_DIGIT = 10;
+    
+    public static int n;
+    public static int[] arr = new int[MAX_N];
 
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-
-        for(int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        radixSort(arr, n);
-
-        for(int m : arr) {
-            System.out.print(m + " ");
-        }
-    }
-
-    public static int[] radixSort(int arr[], int n) {
+    public static void radixSort() {
         int p = 1;
-        for(int i = 0; i < n; i++) {
-            List<Integer>[] li = new ArrayList[10];
-            for(int j = 0; j < 10; j++) {
-                li[j] = new ArrayList<>();
-            }
-
-            for(int j = 0; j < n; j++) {
-                int digit = (arr[j] / p) % 10;
-                li[digit].add(arr[j]);
+        for(int pos = 0; pos < MAX_K; pos++) {
+            ArrayList<Integer>[] arrNew = new ArrayList[MAX_DIGIT];
+            for(int i = 0; i < MAX_DIGIT; i++)
+                arrNew[i] = new ArrayList<>();
+            
+            for(int i = 0; i < n; i++) {
+                int digit = (arr[i] / p) % 10;
+                arrNew[digit].add(arr[i]);
             }
 
             int index = 0;
-            for(int j = 0; j < 10; j++) {
-                for(int k = 0; k < li[j].size(); k++) {
-                    arr[index++] = li[j].get(k);
-                }
-            }
-
+            for(int i = 0; i < MAX_DIGIT; i++)
+                for(int j = 0; j < arrNew[i].size(); j++)
+                    arr[index++] = arrNew[i].get(j);
+            
             p *= 10;
         }
-        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 입력
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        radixSort();
+
+        for(int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
     }
 }
