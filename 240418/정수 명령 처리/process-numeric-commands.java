f0732d8c3a588ba46d538033e24ca061ea3fbf06
pie <104147789+pie2457@users.noreply.github.com>
2024-04-18 16:16:43 +0900

    Title: 정수 명령 처리, Time: 399ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 240418/정수 명령 처리/process-numeric-commands.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < n; i++) {
+            String s = sc.next();
+
+            if (s.equals("push")) {
+                int m = sc.nextInt();
+                stack.push(m);
+            } else if (s.equals("size")) {
+                System.out.println(stack.size());
+            } else if (s.equals("empty")) {
+                System.out.println(stack.empty() ? 1 : 0);
+            } else if (s.equals("pop")) {
+                System.out.println(stack.pop());
+            } else if (s.equals("top")) {
+                System.out.println(stack.peek());
+            }
+        }
+    }
+}
