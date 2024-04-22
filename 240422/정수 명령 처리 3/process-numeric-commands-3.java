5809f56b554ee0f49d5dff69e26ed21adda4854c
pie <104147789+pie2457@users.noreply.github.com>
2024-04-22 13:55:49 +0900

    Title: 정수 명령 처리 3, Time: 464ms, Memory: 22MB, Status: Passed - CodeTree

--- 240422/정수 명령 처리 3/process-numeric-commands-3.java
+++ 240422/정수 명령 처리 3/process-numeric-commands-3.java
@@ -12,26 +12,19 @@ public class Main {
             if (s.equals("push_front")) {
                 int m = sc.nextInt();
                 dq.addFirst(m);
-                continue;
             } else if (s.equals("push_back")) {
                 int m = sc.nextInt();
                 dq.addLast(m);
-                continue;
             } else if (s.equals("pop_front")) {
                 System.out.println(dq.pollFirst());
-                continue;
             } else if (s.equals("pop_back")) {
                 System.out.println(dq.pollLast());
-                continue;
             } else if (s.equals("size")) {
                 System.out.println(dq.size());
-                continue;
             } else if (s.equals("empty")) {
                 System.out.println(dq.isEmpty() ? 1 : 0);
-                continue;
             } else if (s.equals("front")) {
                 System.out.println(dq.peekFirst());
-                continue;
             } else if (s.equals("back")) {
                 System.out.println(dq.peekLast());
             }
